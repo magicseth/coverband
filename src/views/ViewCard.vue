86c5e557c72842dd6d3512aff9784d835f6bea09
Seth Raphael <git@magicseth.com>
2024-09-30 18:07:57 -0700

    give devices names

--- src/views/ViewCard.vue
+++ src/views/ViewCard.vue
@@ -18,8 +18,71 @@ const { id } = useRoute().params
 const { data: card } = useConvexQuery(api.cards.getCardById, { id: id as Id<'cards'> })
 const { mutate: sendPing } = useConvexAction(api.cardsnode.sendPing)
 
+import { ref, onMounted } from 'vue'
+
+const colors = [
+  'Red',
+  'Blue',
+  'Green',
+  'Yellow',
+  'Purple',
+  'Orange',
+  'Pink',
+  'Brown',
+  'Gray',
+  'Black',
+  'White',
+  'Teal',
+  'Maroon',
+  'Navy',
+  'Olive',
+  'Cyan',
+  'Magenta',
+  'Lavender',
+  'Turquoise',
+  'Gold'
+]
+
+const animals = [
+  'Lion',
+  'Tiger',
+  'Bear',
+  'Wolf',
+  'Fox',
+  'Eagle',
+  'Hawk',
+  'Dolphin',
+  'Shark',
+  'Elephant',
+  'Giraffe',
+  'Penguin',
+  'Koala',
+  'Kangaroo',
+  'Octopus',
+  'Panther',
+  'Cheetah',
+  'Gorilla',
+  'Owl',
+  'Raccoon'
+]
+
+const codename = ref('')
+
+onMounted(() => {
+  let storedCodename = localStorage.getItem('userCodename')
+  if (!storedCodename) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)]
+    storedCodename = `${randomColor} ${randomAnimal}`
+    localStorage.setItem('userCodename', storedCodename)
+  }
+  codename.value = storedCodename
+})
+
 watch(card, () => {
-  sendPing({ message: 'tarot card played ' + card?.value?.title })
+  sendPing({
+    message: codename.value + ' played ' + card?.value?.title + ' url ' + window.location.href
+  })
 })
 </script>
 
